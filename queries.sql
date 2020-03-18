c96f49179a720efc676d29efb23ad0fb21c6bc8b
Laura Creason <creason4@gmail.com>
2020-03-17 21:18:05 -0500

    Part 1 and 2

--- queries.sql
+++ queries.sql
@@ -1,6 +1,15 @@
 ## Part 1: Test it with SQL
 
+SELECT column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'job'
+ORDER BY ordinal_position;
+
 ## Part 2: Test it with SQL
+
+SELECT name
+FROM employer
+WHERE location="Saint Louis City";
 
 ## Part 3: Test it with SQL
 
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,50 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message="Location needed longer than 3 characters")
+    @Size(min=3, max=100)
+    private String location;
 
+    @OneToMany
+    @JoinColumn(name="employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
+    public Employer(@NotBlank @Size(min=3, max=100)
+                    String location, List<Job> jobs){
+        this.location = location;
+        this.jobs = jobs;
+    }
+
+    public Employer() {
+
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 }
